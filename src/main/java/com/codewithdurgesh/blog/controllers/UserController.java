a43eb8f45ae316dded069badbd2fa31b65a94795
Shivani-manikpure <shivanimanikpure27@gmail.com>
2023-09-28 20:50:50 +0530

    .

---
+++ src/main/java/com/codewithdurgesh/blog/controllers/UserController.java
@@ -0,0 +1,60 @@
+package com.codewithdurgesh.blog.controllers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codewithdurgesh.blog.payloads.ApiResponse;
+import com.codewithdurgesh.blog.payloads.UserDto;
+import com.codewithdurgesh.blog.services.Userservice;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+
+	
+	@Autowired
+	private Userservice userService;
+	
+	@PostMapping
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+		UserDto createUserDto = this.userService.createUser(userDto);
+		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
+		UserDto updatedUserDto = userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updatedUserDto);
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId) {
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<UserDto>> getAllUser() {
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId) {
+		this.userService.deleteUser(userId);
+//		return new ResponseEntity(Map.of("message", "User Deleted Successfully"),HttpStatus.OK);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted Successfully",true, HttpStatus.OK),HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/codewithdurgesh/blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.codewithdurgesh.blog.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import com.codewithdurgesh.blog.payloads.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+		String message = ex.getMessage();
+		ApiResponse apiResponse = new ApiResponse(message,false, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+	}
+}
