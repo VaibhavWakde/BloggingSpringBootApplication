fd2fe6fb0a247ac5728d08961f0f09918bb50b4f
Vaibhav <vaibhav.wakde@kalyanistudio.com>
2023-10-30 17:22:20 +0530

    Implement Basic Auth

--- src/main/java/com/codewithdurgesh/blog/BlogAppApis1Application.java
+++ src/main/java/com/codewithdurgesh/blog/BlogAppApis1Application.java
@@ -1,12 +1,17 @@
 package com.codewithdurgesh.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogAppApis1Application {
+public class BlogAppApis1Application implements CommandLineRunner {
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApis1Application.class, args);
@@ -17,4 +22,9 @@ public class BlogAppApis1Application {
 		return new ModelMapper();
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("abcdd"));
+	}
 }
---
+++ src/main/java/com/codewithdurgesh/blog/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.codewithdurgesh.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.codewithdurgesh.blog.security.CustomUserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService customeDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(this.customeDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
